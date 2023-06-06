d5a2d5f3e678637d58bbfb6ec81985e118b5cfa6
DiegoArr09 <diegoarrigoni12@gmail.com>
2023-06-06 12:13:22 -0300

    Prueba 01 - Register
    Se hace la pagina de register para empezar a probar el cambio de views a front

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import Register from "./pages/Register";
 import "./App.css";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <Register />
+    </>
+  );
 }
 
 export default App;
---
+++ src/pages/Register.jsx
@@ -0,0 +1,98 @@
+import "./Register_Login.css";
+
+function Register() {
+  return (
+    <>
+      <section className="container-fluid container-register">
+        <div className="col-12 register-card-container">
+          <div className="card mb-3 register-card">
+            <div className="row g-0 register-container py-0">
+              <div className="d-none d-md-inline col-md-5 col-lg-7 welcome-container">
+                <img
+                  src="twitter-logo-white.svg"
+                  className="img-fluid rounded-start logo-welcome"
+                  alt="imagen"
+                />
+                <h5 className="welcome-text card-text">
+                  Hi! Welcome to Twitter Clone 👋
+                </h5>
+              </div>
+              <div className="col-sm-12 col-md-7 col-lg-5 right-container">
+                <div className="px-sm-5 card-body register-form-container">
+                  <h2 className="card-title p-1">Sign Up</h2>
+                  <p className="card-text p-1">
+                    Create an account and start using Twitter
+                  </p>
+                  <form
+                    action="/register"
+                    method="POST"
+                    enctype="multipart/form-data"
+                  >
+                    <input
+                      className="form-control my-2 py-2"
+                      type="text"
+                      id="firstname"
+                      name="firstname"
+                      required
+                      placeholder="First Name"
+                    />
+                    <input
+                      className="form-control my-2 py-2"
+                      type="text"
+                      id="lastname"
+                      name="lastname"
+                      required
+                      placeholder="Last Name"
+                    />
+                    <input
+                      className="form-control my-2 py-2"
+                      type="text"
+                      id="username"
+                      name="username"
+                      required
+                      placeholder="Username"
+                    />
+                    <input
+                      type="email"
+                      className="form-control my-2 py-2"
+                      id="email"
+                      name="email"
+                      placeholder="Email"
+                      required
+                    />
+                    <input
+                      className="form-control my-2 py-2"
+                      type="password"
+                      id="password"
+                      name="password"
+                      required
+                      placeholder="Password"
+                    />
+                    <input
+                      className="form-control my-2 py-2"
+                      type="file"
+                      id="avatar"
+                      name="avatar"
+                    />
+
+                    <button type="submit" className="btn-lb sign-up">
+                      Sign Up
+                    </button>
+                    <small className="sign-text">
+                      Already have an account?{" "}
+                      <a href="/login" className="sign-link">
+                        Sign in
+                      </a>
+                    </small>
+                  </form>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+}
+
+export default Register;
